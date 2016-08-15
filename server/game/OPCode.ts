6dd1eae5a5548e02c4e0cb4da5c99883528705c9
Santhaya <santhayadesign@gmail.com>
2016-08-15 22:15:25 +0200

    WorldServer, OPCode

---
+++ server/game/OPCode.ts
@@ -0,0 +1,11 @@
+/**
+ * Operation codes
+ *
+ * Mělo by být sdíleno mezi serverem a klientem
+ *
+ * @type {{}}
+ */
+export const OPCODE = {
+    PLAYER_CONNECTED: "playerConnected",
+    PLAYER_MOVE: "playerMove"
+};
--- server/game/WorldServer.ts
+++ server/game/WorldServer.ts
@@ -1,21 +1,45 @@
+import * as uuid from "node-uuid";
 import {Player} from "./Player";
 
+/**
+ * WorldServer
+ *
+ * Střed všeho dění, zde probíhá vše důležité.
+ * Připojování klientů, transform na Player a update stavu, zpracování
+ * požadavků, sync na klienty.
+ */
 export class WorldServer {
 
     protected players: Player[] = [];
     
     constructor(private socketServer: SocketIO.Server) {}
 
+    /**
+     * Připojení klienta
+     * 
+     * @param client
+     */
     public onPlayerLogged(client: SocketIO.Socket): void {
-   
+
     }
 
+    /**
+     * Požadavek klienta
+     *
+     * @param player
+     * @param msg
+     */
     public onMessage(player: Player, msg: string): void {
     
     }
 
+    /**
+     * Generuje unikátní identifikátor (random)
+     *
+     * @returns {string}
+     */
     private generateUUID(): string {
-      
+        return uuid.v4();
     }
 
 }
